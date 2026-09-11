926c1f6dcdc94723c1c16fd9df960217818fa97a
theogturkishstreetperformer <patiernobrayden24@gmail.com>
2026-09-11 10:37:56 -0400

    Add files

---
+++ active.js
@@ -0,0 +1,13 @@
+function showDescription(btn) {
+    const card = btn.closest('.card');
+    const description = card.querySelector('.description');
+
+    description.classList.toggle('show');
+}
+
+function showDescription(btn) {
+    const card = btn.closest('.card');
+    const description = card.querySelector('.description');
+
+    description.classList.toggle('show');
+}
